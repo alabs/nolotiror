5ccbd4162c43321045dc68c5d49be7e8c2ebc0cb
Antonio Pardo <apardo@alabs.org>
2013-05-21 22:26:20 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nolotiro::Application.config.secret_token = '57a7960e4a936d396568bdd9c1fd4f46e91e92955e8fff27e8470349ecfd2c83e6333ecabc96efa83f5dcb5143ff3ef0b1af2128b1bfa5d25fa3aca243f130b0'
